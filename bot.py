9abfd2f4b23f3ce3a570b78cac82d3b8f1963570
Mark Proskurin <growberry.mark@gmail.com>
2022-09-06 12:47:29 +0300

    My first Bot

---
+++ bot.py
@@ -0,0 +1,35 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+# PROXY = {'proxy_url': 'socks5h://t3.learn.python.ru:1080',
+#     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+def greet_user(update, context):
+    print("Вызван/start")
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+    
+def talk_to_me(update, context):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info("Бот стартовал")
+    # Командуем боту начать ходить в Telegram за сообщениями
+    mybot.start_polling()
+    # Запускаем бота, он будет работать, пока мы его не остановим принудительно
+    mybot.idle()
+    # Вызываем функцию main() - именно эта строчка запускает бота
+if __name__ == "__main__":
+    main()
